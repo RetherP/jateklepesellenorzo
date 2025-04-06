4d6e8ca9a254ea567caeb85d35b3cfb104f2949d
RetherP <vbelya07@gmail.com>
2025-04-06 14:12:04 +0200

    Added a method to the controller to get the current player position

--- BACKEND/BACKEND/Controllers/GameApiController.cs
+++ BACKEND/BACKEND/Controllers/GameApiController.cs
@@ -1,6 +1,38 @@
-﻿namespace BACKEND.Controllers
+﻿using BACKEND.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
+
     public class GameApiController
     {
+        [HttpGet("{moves}")]
+        public Game ValidateMoves([FromBody] Game g, string moves)
+        {
+            Game res = new Game();
+            Tuple<int, int> currentLoc = CurrentLocation(g);
+            if(currentLoc.Item1 != -1 && currentLoc.Item2 != -1)
+            {
+                //some logic here
+            }
+            return res;
+        }
+        public Tuple<int,int> CurrentLocation(Game g)
+        {
+            Tuple<int, int> res = new Tuple<int, int>(-1,-1);
+            for (int i = 0; i < g.CurrentState.GetLength(0); i++)
+            {
+                for (int j = 0; j < g.CurrentState.GetLength(1); j++)
+                {
+                    if (g.CurrentState[i,j] == '█')
+                    {
+                        return (new Tuple<int, int>(i, j));
+                    }
+                }
+            }
+            return res;
+        }
     }
 }
--- BACKEND/BACKEND/Models/Game.cs
+++ BACKEND/BACKEND/Models/Game.cs
@@ -2,7 +2,7 @@
 {
     public class Game
     {
-        public char[] CurrentState { get; set; }
+        public char[,] CurrentState { get; set; }
         public int NumOfCoins { get; set; }
     }
 }
